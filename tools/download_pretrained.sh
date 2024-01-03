9e2084fed541a2cc614421e217a11922aa968da5
Han Keceli <hankeceli97@gmail.com>
2024-01-04 00:03:41 +0100

    some alignments

--- tools/download_pretrained.sh
+++ tools/download_pretrained.sh
@@ -1,10 +1,10 @@
 mkdir pretrained &&
 cd pretrained &&
-wget https://bevfusion.mit.edu/files/pretrained_updated/bevfusion-det.pth &&
-wget https://bevfusion.mit.edu/files/pretrained_updated/bevfusion-seg.pth &&
-wget https://bevfusion.mit.edu/files/pretrained/lidar-only-det.pth &&
-wget https://bevfusion.mit.edu/files/pretrained/lidar-only-seg.pth &&
-wget https://bevfusion.mit.edu/files/pretrained_updated/camera-only-det.pth &&
-wget https://bevfusion.mit.edu/files/pretrained_updated/camera-only-seg.pth &&
-wget https://bevfusion.mit.edu/files/pretrained_updated/swint-nuimages-pretrained.pth
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained_updated/bevfusion-det.pth &&
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained_updated/bevfusion-seg.pth &&
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained_updated/lidar-only-det.pth &&
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained/lidar-only-seg.pth &&
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained/camera-only-det.pth &&
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained_updated/camera-only-seg.pth &&
+wget https://hanlab18.mit.edu/projects/bevfusion/files/pretrained_updated/swint-nuimages-pretrained.pth
 
